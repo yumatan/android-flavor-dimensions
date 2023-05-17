3a1cfa3bc5b8e0b0d6a47f4b3691cfb17e62d7d3
yumatan <yuma.tanaka1129@gmail.com>
2023-05-17 22:19:27 +0900

    chore

--- app/src/main/java/com/example/android_flavor_dimensions/MainActivity.kt
+++ app/src/main/java/com/example/android_flavor_dimensions/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.android_flavor_dimensions.ui.theme.Android_flavor_dimensionsTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +23,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Greeting(this.getString(R.string.target_text))
                 }
             }
         }
@@ -32,8 +33,9 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        text = name,
+        modifier = modifier,
+        fontSize = 60.0.sp
     )
 }
 
